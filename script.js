bfa8ce603a8eb5409f6ec33e9cbd86c4ae620b41
rodgers <rodgerstarrodgers@gmail.com>
2025-01-19 03:42:44 +0300

    Rock Paper Scissors Game; build computer choice and get result function

--- script.js
+++ script.js
@@ -1,50 +1,85 @@
-console.log('hello')
+// console.log('hello')
+//
+// let titleDiv =
+//     document.getElementById('titleDiv')
+//
+// console.log(titleDiv)
+// console.log('before:', titleDiv.innerText)
+// let message = 'am here js!';
+// titleDiv.innerText = message;
+//
+//
+// console.log('after:', titleDiv.innerText)
+//
+// titleDiv.innerHTML = `<p> ${message}</p>`
+//
+// titleDiv.style.backgroundColor = 'red';
+//
+// let redDiv = document.getElementById('red')
+// let yellowDiv = document.getElementById('yellow')
+// let greenDiv = document.getElementById('green')
+//
+// redDiv.onclick = () => console.log('red')
+// yellowDiv.onclick = () => console.log('yellow')
+// greenDiv.onclick = () => console.log('green')
+//
+// const squares = document.querySelectorAll('.colorSquare')
+// // console.log(squares)
+//
+// // console.log(squares[0].value)
+// const timesClicked = {'red': 0, 'yellow':0, 'green': 0,}
+// squares.forEach(square => {
+//     square.onclick = () => {
+//         timesClicked[square.value] += 1
+//         square.innerText = timesClicked[square.value]
+//     }
+// })
+//
+// function clearScores () {
+//     timesClicked.red = 0
+//     timesClicked.green = 0
+//     timesClicked.yellow = 0
+//     squares.forEach(square => {
+//         square.innerText = 0
+//     })
+// }
+//
+//
+// const clearGameDiv = document.getElementById('clearGame')
+// clearGameDiv.onclick = () => clearScores()
+//
 
-let titleDiv =
-    document.getElementById('titleDiv')
+// const randomSelectFromArray = (fruits) => {
+//     const randomNumber =
+//         Math.floor(Math.random() * fruits.length)
+//
+//     return fruits[randomNumber]
+// }
+//
+// fruits = ['banna', 'apple', 'orange', 'grapes']
+// console.log(randomSelectFromArray(fruits))
 
-console.log(titleDiv)
-console.log('before:', titleDiv.innerText)
-let message = 'am here js!';
-titleDiv.innerText = message;
-
-
-console.log('after:', titleDiv.innerText)
-
-titleDiv.innerHTML = `<p> ${message}</p>`
-
-titleDiv.style.backgroundColor = 'red';
-
-let redDiv = document.getElementById('red')
-let yellowDiv = document.getElementById('yellow')
-let greenDiv = document.getElementById('green')
-
-redDiv.onclick = () => console.log('red')
-yellowDiv.onclick = () => console.log('yellow')
-greenDiv.onclick = () => console.log('green')
-
-const squares = document.querySelectorAll('.colorSquare')
-// console.log(squares)
-
-// console.log(squares[0].value)
-const timesClicked = {'red': 0, 'yellow':0, 'green': 0,}
-squares.forEach(square => {
-    square.onclick = () => {
-        timesClicked[square.value] += 1
-        square.innerText = timesClicked[square.value]
-    }
-})
-
-function clearScores () {
-    timesClicked.red = 0
-    timesClicked.green = 0
-    timesClicked.yellow = 0
-    squares.forEach(square => {
-        square.innerText = 0
-    })
+function getComputerChoice () {
+    const rpsChoice = ['Rock', 'Paper', 'Scissors']
+    const randNumber = Math.floor(Math.random() * 3)
+    return rpsChoice[randNumber]
 }
 
+function getResult(playerChoice, computerChoice) {
 
-const clearGameDiv = document.getElementById('clearGame')
-clearGameDiv.onclick = () => clearScores()
+    let  score;
+
+    if (playerChoice === computerChoice) {
+        score = 0
+    } else if (playerChoice === 'Rock' && computerChoice === 'Scissors') {
+        score = 1
+    } else if (playerChoice === 'Paper' && computerChoice === 'Rock') {
+        score = 1
+    } else if (playerChoice === 'Scissors' && computerChoice === 'Paper') {
+        score = 1
+    } else {
+        score = -1
+    }
+}
+
 
